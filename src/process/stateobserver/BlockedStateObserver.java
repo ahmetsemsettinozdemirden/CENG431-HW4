4d2bff1c26722d4e47caa0db0d09fac7575509d1
elof <elif@uncosoft.com>
2018-12-23 23:01:21 +0300

    Added missing observer classes

---
+++ src/process/stateobserver/BlockedStateObserver.java
@@ -0,0 +1,11 @@
+package process.stateobserver;
+
+import process.Observable;
+
+public class BlockedStateObserver implements Observer {
+
+    @Override
+    public void update(Observable observable) {
+
+    }
+}
